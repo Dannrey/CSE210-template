133b3a65dd82c5714d6d2bef97454d48ab27feac
Danniel Reynolds <danniel.r@aol.com>
2024-05-08 22:47:16 -0800

    So, inside the sandbox file I spent all day messing with classes, and the finished product looks like one of the examples, but I eventually figured the syntax out. That was not easy.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -7,3 +7,49 @@ class Program
         Console.WriteLine("Hello Sandbox World!");
     }
 }
+
+
+// My experiment with classes.
+// So, I actually managed to figure it out, and I'm glad about it.
+
+// public class Person
+// {
+//     public string _givenName = "";
+//     public string _familyName = "";
+
+//     public Person()
+//     {
+//     }
+
+//     public void ShowEasternName()
+//     {
+//         Console.WriteLine($"{_familyName}, {_givenName}");
+//     }
+
+//     public void ShowWesternName()
+//     {
+//         Console.WriteLine($"{_givenName} {_familyName}");
+//     }
+// }
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Person person = new Person();
+//         person._givenName = "Danniel";
+//         person._familyName = "Reynolds";
+//         person.ShowWesternName();
+//         person.ShowEasternName();
+
+//         Console.WriteLine();
+
+//         Person person2 = new Person();
+//         person2._givenName = "Teddy";
+//         person2._familyName = "Roosevelt";
+//         person2.ShowWesternName();
+//         person2.ShowEasternName();
+//     }
+// }
+
+// So, as it turns out, you need to define custom classes outside of the thing designated "class program".
+// Aside from that, I do believe that I figured it out.

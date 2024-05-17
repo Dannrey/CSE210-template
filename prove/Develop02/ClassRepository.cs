dfc048e1c3fac402839cf5250cbde055e4cc41d6
Danniel Reynolds <danniel.r@aol.com>
2024-05-17 15:18:29 -0800

    I do believe that this is the final version of the project, and I reformatted the ClassRepository.cs file so that the names made more sense for what they actually did.

--- prove/Develop02/ClassRepository.cs
+++ prove/Develop02/ClassRepository.cs
@@ -3,11 +3,11 @@ using System.IO;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 
-// So, here is where I defined my custom classes and functions.
-// It was necessary to do this in order to allow me to program with my easily reusable functions,
-// And I wanted to try something different with the format of my files. After all, in C languages, you don't
-// actually need to indent lines in order for the code to work. I'm using the indentations to help keep track of
-// what does what.
+/* So, here is where I defined my custom classes and functions.
+It was necessary to do this in order to allow me to program with my easily reusable functions,
+And I wanted to try something different with the format of my files. After all, in C languages, you don't
+actually need to indent lines in order for the code to work. I'm using the indentations to help keep track of
+what does what.*/
 public class FileEditor {
     // First, I'll define variables that the function will use:
     public string _fileName;  // This contains the name of the file
@@ -15,11 +15,8 @@ public class FileEditor {
     public string _addTime;   // This variable holds the time in a string
     public string _addEntryPrompt;   // This variable holds the propmt that was given to the user
     public string _addEntryContent;   // This varible holds the content of the journal entry
-    public List<string> _saveThis;   // This is vital for the program to actually keep the previous journal entries.
-    // I don't know why, but my first attempts always deleted what was already inside the text file, so I added this
-    // to maintain what was already there. It might take up a lot of temporary computer storage space but it was the
-    // only and fastest way that I could concieve to fix the problem.
-
+    public List<string> _saveThis;   // This variable makes sure that previous entries do not get deleted.
+    // Until I can figure out why this happens otherwise, I'll be using this.
     public FileEditor()
     {
     }
@@ -36,7 +33,7 @@ public class FileEditor {
         
         List<string> KeepThis = new List<string>();
         foreach (string line in lines) {
-            string[] parts = line.Split("∫");
+            string[] parts = line.Split("∞");
             foreach (string part in parts) {
                 KeepThis.Add(part);
             }
@@ -52,11 +49,15 @@ public class FileEditor {
         // return KeepThis;
     }
 
-
     public List<string> KeepJournalEntries() {
+        /*
+        Name: KeepJounrnalEntries
+        Variables: None
+        Returns: keepThis, a string list.
+        */
         string[] lines = System.IO.File.ReadAllLines(_fileName);
-    
         List<string> LineList = new List<string>();
+
         
         List<string> KeepThis = new List<string>();
         foreach (string line in lines) {
@@ -72,67 +73,99 @@ public class FileEditor {
         return KeepThis;
     }
 
-
-    public void AppendThis(List<string> ListX) {
+    public void AppendThis() {
         using (StreamWriter OutputFile = new StreamWriter(_fileName)) {
             int iteration = 0;
-            foreach (string item in ListX) {
-                OutputFile.WriteLine(ListX[iteration]);
+            foreach (string item in _saveThis) {
+                OutputFile.WriteLine(_saveThis[iteration].Trim());
                 iteration += 1;
             }
-        
-            // OutputFile.WriteLine("∫");
             OutputFile.WriteLine("\n"+_addDate.Trim());
             OutputFile.WriteLine(_addTime.Trim());
             OutputFile.WriteLine(_addEntryPrompt.Trim());
             OutputFile.WriteLine(_addEntryContent.Trim());
-            OutputFile.WriteLine("∫".Trim());
+            OutputFile.Write("∞".Trim());
         }
     }
-    
 }
 
-public class UserSelection {
+public class Entry {
     // This class will contain a selection of functions designed to be used on a key number being entered into
     // the corresponding question in the main program.
     
     // I also have a few variables that need to be created in order for this part of the program to work.
     // These are defined below:
-    public string _rememberThis;
+    public string _entryContent;
+    public string _dateString;
+    public string _timeString;
+    public string _prompt;
 
-
-    public UserSelection()
+    public Entry()
     {
     }
 
-    public string Selection1() {
-        // Selection1 will take input from the user and store it inside a variable, ready to be appended to the
+    public string ObtainInformation() {
+        // The ObtainInformation() function will take input from the user and store it inside a variable, ready to be appended to the
         // text file.
-        string RecorderEso = "";
+        string EntryData = "";
         string UserResponse = "";
-        string TheThirdOne = "";
+        string AddThis = "";
         Console.WriteLine("Hit enter when you are ready to move onto a new line. Type 'QUIT()' to end this part of the program.");
-        while (UserResponse != "QUIT()") {
-            Console.Write("");
+        while (UserResponse != "QUIT()") {       // This while loop and following if statement are part of my creativity. This basically allows the user to format
+            Console.Write("");                   // their entry, to an extent, by allowing you to enter a new line in. To exit, you type QUIT() in all caps and with
+            UserResponse = Console.ReadLine();   // parenthesis. This function will store letters and the new lines.
             if (UserResponse != "QUIT()") {
-                UserResponse = Console.ReadLine();
-                TheThirdOne = RecorderEso;
-                RecorderEso = $"{TheThirdOne}\n{UserResponse}";
-            }
-            else {
-                string EmptyVariable = Console.ReadLine();
-            }
-            
+                AddThis = EntryData;
+                EntryData = $"{AddThis}\n{UserResponse}";
+            }            
         }
-        return RecorderEso;
+        EntryData = $"{_timeString}\n{_dateString}\n{EntryData}";
+
+        return EntryData;
     }
 
-    public void Selection2() {
-        // Selection2 will display what was last written by the user.
-        Console.WriteLine(_rememberThis);
+    public void DisplayInformation() {
+        // DisplayInformation will display what was last written by the user.
+        Console.WriteLine(_entryContent);
         Console.Write("Hit the enter key when you are ready to continue: ");
-        string EmptyVariable = Console.ReadLine();
+        Console.ReadLine();
+    }
+}
+
+public class RandomizedPrompt {
+    /* This class will only have a member variable and a function.
+    Its purpose is to create and return a randomized variable each time it is called. 
+    */
+
+    public List<string> _randomizedPromptList;
+    public string _randomizedPrompt;
+
+    public RandomizedPrompt() 
+    {
     }
 
+
+    // Hey, here's a fun idea; what if I made it so that the user can enter custom prompts? That would require the use of anoher text file specifically with that.
+    public List<string> GenerateRandomizedPrompt() {
+        List<string> RandomList = new List<string>();
+        RandomList.Add("What was the most exciting thing you did today?");
+        RandomList.Add("What was the worst thing that happened to you today?");
+        RandomList.Add("What was the best thing that you ate today?");
+        RandomList.Add("Where did you go today?");
+        RandomList.Add("What did you do in your offtime today?");
+        RandomList.Add("What was the nicest thing you did for another today?");
+        RandomList.Add("What was the nicest thing another did for you today?");
+        RandomList.Add("What do you have to look forward to tomorrow?");
+        RandomList.Add("What was the funniest thing you saw today?");
+        RandomList.Add("Did you meet someone new today?");
+        return RandomList;
+    }
+    
+    public string SelectRandomizedPrompt() {
+        Random randomGenerator = new Random();
+        int RandomizedPlaceSelection = randomGenerator.Next(0,_randomizedPromptList.Count);
+        string RandomizedSelection = _randomizedPromptList[RandomizedPlaceSelection];
+        return RandomizedSelection;
+    }
 
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,125 +1,137 @@
+/*
+File name: Program.cs
+Author: Danniel Reynolds
+Date: 5/11/2024 - 5/??/2024
+Purpose: 
+*/
+
+/*
+So, in this file I have several features added for the craetiviy portion of the assignment. 
+First and formost, I gave the program a name to call itself. That alone I would not classify
+for the entire portion of the creativity of the assignment, but I added more.
+I also made the program so that it will generate a new prompt each time the user selects option 1, Write a new entry,
+Option 1 also presents the user with the option to put new lines inside the middle of their entry rather than 
+just making one long, continuous string. This is a neat feature, I think. After that, I also designed the 
+program to keep track of the time that an entry was written, and my thought was because someone might come back 
+later in the same day and add another entry.
+After that, I also provied some formatting for when the file itself is ran, such as while the program is saving,
+I added some code to cause the display to take a little time to generate a response when you select option 3, save entry.
+I learned this bit of code from one of my team members, Jeremy Untch.
+I also used the .Trim() method in the second option, Display current entry. I went online and learned the .Trim() method from: learn.microsoft.com
+I also did something similar in the fourth option that the user can select, which is the Save entry option.
+I also added some code to handle any input that does not relate to one of the selections. This alerts the user to
+the fact that they did not actually enter a valid option.
+Lastly, 
+*/
+
 using System;
 using System.IO;
 using System.Collections.Generic;
 
 // Okay, here is the Program class. This will be where the magic happens inside this file.
 class Program {
-static void Main(string[] args) {
-// Global Variables:
-string MyDoc = "MyTextFile.txt";
+    static void Main(string[] args) {
+        // Global Variables:
+        string MyDoc = "MyTextFile.txt";
+
+        // FileEditor class references:
+        FileEditor Journal = new FileEditor();
+        Journal._fileName = MyDoc;
+        Journal._addDate = "";
+        Journal._addTime = "";
+        Journal._saveThis = Journal.KeepJournalEntries();
+
+        DateTime theCurrentTime = DateTime.Now;
+        // Entry Class references:
+        Entry entry = new Entry();
+        entry._entryContent = "You have not entered anything yet!"; 
+        entry._dateString = theCurrentTime.ToShortDateString();
+        entry._timeString = theCurrentTime.ToShortTimeString();
+        entry._prompt = "emptry prompt";
 
 
-// DateTime class reference:
-DateTime theCurrentTime = DateTime.Now;
+        // RandomizedPrompt Class references:
+        RandomizedPrompt Prompt = new RandomizedPrompt();
+        Prompt._randomizedPromptList = Prompt.GenerateRandomizedPrompt();
 
+        // I always include something like this when I create a larger program for my class.
+        Console.WriteLine("Hello and welcome! I am the Journal Application and Reminder Establishment Device, but you may call me\nJ.A.R.E.D.");
+        Console.WriteLine("My purpose today is to provide you with a prompt to assist you in keeping a consistent journal and to save any\nentry that you might make.");
+        Console.WriteLine("Each of the selection options will have their own individual instruction that will be displayed when you select one,\n");
+        Console.WriteLine("and you can select all options multiple times (aside from option 5, since that closes the program).");
+        Console.WriteLine("My methods to assist you today are:\n");
 
-// DisplayThese class references:
-FileEditor Journal = new FileEditor();
-Journal._fileName = MyDoc;
-Journal._addDate = theCurrentTime.ToShortDateString();
-Journal._addTime = theCurrentTime.ToShortTimeString();
+        string UserInput;
+        do {
+            Console.WriteLine("\t1. Write a new entry\n\t2. Display current entry\n\t3. Load previous entries\n\t4. Save entry\n\t5. Quit\n");
+            Console.Write("Please select and type a numeric option from the list: ");
+            UserInput = Console.ReadLine();
+            if (UserInput == "1") {
+                Console.WriteLine("This option will allow you to create a new entry.");
+                Console.WriteLine("Before you can enter your entry, I need to provide you with a randomly selected prompt.");
+                Console.WriteLine("Again, it is up to you whether or not to head the prompt, but nevertheless, here it is:");
 
-// This will be a combination of an instance of the random class and a reference to my DisplayThese class.
-Random randomGenerator = new Random();
-List<string> RandomList = new List<string>();
-RandomList.Add("What was the most exciting thing you did today?");
-RandomList.Add("What was the worst thing that happened to you today?");
-RandomList.Add("What was the best thing that you ate today?");
-RandomList.Add("Where did you go today?");
-RandomList.Add("What did you do in your offtime today?");
+                Prompt._randomizedPrompt = Prompt.SelectRandomizedPrompt();
+                Journal._addEntryPrompt = Prompt._randomizedPrompt;
+                entry._prompt = Journal._addEntryPrompt;
+                Console.WriteLine(Prompt._randomizedPrompt);
 
+                entry._dateString = theCurrentTime.ToShortDateString();
+                entry._timeString = theCurrentTime.ToShortTimeString();
 
+                entry._entryContent = entry.ObtainInformation();
+                Journal._addEntryContent = entry._entryContent;
+                Console.WriteLine();
+            }
+            else if (UserInput == "2") {
+                if (Journal._addEntryContent.Trim() == "") {
+                    Console.WriteLine("I'm sorry, but you didn't actually type anything.");
+                }
+                else {
+                    entry.DisplayInformation();
+                }
+                Console.WriteLine();
 
-Journal._saveThis = Journal.KeepJournalEntries();
-// Journal.ReadJournalEntries();
-// Journal.AppendThis();
+            }
+            else if (UserInput == "3") {
+                Journal.ReadJournalEntries();
+            }
 
+            else if (UserInput == "4") {
+                if (Journal._addEntryContent == "You have not entered anything yet!"||Journal._addEntryContent.Trim() =="") {
+                    Console.WriteLine("You have not entered anything yet! Please type something");
+                }
+                else {
+                    Journal._saveThis = Journal.KeepJournalEntries();
+                    Journal._addEntryContent = entry._entryContent;
+                    Journal.AppendThis();
+                    Console.Write(".");
+                    Thread.Sleep(1000);
+                    Console.Write(".");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("."); 
+                    Thread.Sleep(1000);
+                    Console.Write("Saving");
+                    Thread.Sleep(1000);
+                    Console.Write(".");
+                    Thread.Sleep(1000);
+                    Console.Write(".");
+                    Thread.Sleep(1000);
+                    Console.WriteLine(".");
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Save Complete, hit Enter when you are ready to continue.");
+                    Console.ReadLine();
+                }
+            }
+            else if (UserInput == "5") {
+                Console.WriteLine("See you soon!");
+            }
+            else {
+                // I wasn't told to put this into the program specifically, but I like to have something in the event that the user entered something that was not part of the selection list.
+                Console.WriteLine("Sorry, but that is not one of the available options. Please try again.");
+            }
+            Console.WriteLine();
+        } while (UserInput != "5");
 
-// UserSelection class refenrences:
-UserSelection Choice = new UserSelection();
-Choice._rememberThis = "You have not entered anything yet!";
-
-// Now that I have the code backbone set up, I can go build the part of the program that the user would actually
-// interact with:
-Console.WriteLine("Hello and welcome! I am the Journal Application and Reminder Establishment Device, but you may call me\nJ.A.R.E.D.");
-Console.WriteLine("My purpose today is to provide you with a prompt to assist you in keeping a consistent journal and to save any\nentry that you might make.");
-Console.WriteLine("Each of the selection options will have their own individual instruction that will be displayed when you select one,\n");
-Console.WriteLine("and you can select all options multiple times (aside from option 5, since that closes the program).");
-Console.WriteLine("My methods to assist you today are:\n");
-// Console.Write("Please select and type a numeric option from the list: ");
-// string UserInput = Console.ReadLine();
-
-
-string UserInput;
-do {
-    Console.WriteLine("\t1. Write a new entry\n\t2. Display current entry\n\t3. Load previous entries\n\t4. Save entry\n\t5. Quit\n");
-    Console.Write("Please select and type a numeric option from the list: ");
-    UserInput = Console.ReadLine();
-    if (UserInput == "1") {
-        Console.WriteLine("This option will allow you to create a new entry.");
-        Console.WriteLine("Before you can enter your entry, I need to provide you with a randomly selected prompt.");
-        Console.WriteLine("Again, it is up to you whether or not to head the prompt, but nevertheless, here it is:");
-        
-        Journal._addDate = theCurrentTime.ToShortDateString();
-        Journal._addTime = theCurrentTime.ToShortTimeString();
-
-        int PlaceSelecter = randomGenerator.Next(0,RandomList.Count);
-        Journal._addEntryPrompt = RandomList[PlaceSelecter];
-        Console.WriteLine($"{Journal._addEntryPrompt}");
-
-        string TransitoryVariable1 = Choice.Selection1();
-
-        // Now, if you pay close attention you might be wondering what is going on here. Well, I'm glad you asked, or 
-        // stopped to read this comment. I noticed that for some reason my code was including "QUIT()" at the end of each 
-        // of my entries, and so, rather than going through and possibly messing up the code in an attempt to remove that,
-        // I made a few lines here to pluck out the QUIT() option for me. It might seem like the long, roundabout way to 
-        // accomplish this task but it seemed the simplest to me and I've done similar things to it before.
-        // In the long run, it would be better to go back and modify the original function itself since that would root out
-        // any future errors if I chose to reuse the code, butfor this single assignment it works perfectly well.
-        List<string> TransitoryList = new List<string>();
-        TransitoryList.Add(TransitoryVariable1);
-        foreach (string item in TransitoryList) {
-            string[] parts = item.Split("QUIT()");
-            Choice._rememberThis = parts[0];
-        }
-        Journal._addEntryContent = Choice._rememberThis;
     }
-    else if (UserInput == "2") {
-        if (Journal._addEntryContent.Trim() == "") {
-            Console.WriteLine("I'm sorry, but you didn't actually type anything.");
-        }
-        else {
-            Choice.Selection2();
-        }
-        
-    }
-    else if (UserInput == "3") {
-        Journal.ReadJournalEntries();
-    }
-
-    else if (UserInput == "4") {
-        if (Journal._addEntryContent == "You have not entered anything yet"||Journal._addEntryContent.Trim() =="") {
-            Console.WriteLine("You have not entered anything yet! Please type something");
-        }
-        else {
-            Journal._saveThis = Journal.KeepJournalEntries();
-            Journal._addEntryContent = Choice._rememberThis;
-            Journal.AppendThis(Journal._saveThis);
-            Console.WriteLine("...");
-            Console.WriteLine("Saving...");
-            Console.WriteLine("Save Complete, Hit enter when you are ready to continue.");
-            Console.ReadLine();
-            
-        }
-    }
-    else if (UserInput == "5") {
-        Console.WriteLine("See you soon!");
-    }
-    else {
-        Console.WriteLine("Sorry, but that is not one of the available options. Please try again.");
-    }
-    Console.WriteLine();
-} while (UserInput != "5");
-
-}
 }

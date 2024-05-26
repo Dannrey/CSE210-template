32fd2ad400ab09f7355c502a971dc80570611f1a
Danniel Reynolds <danniel.r@aol.com>
2024-05-25 21:52:06 -0800

    So, I created a new file in the same folder as the develop3 project, and it is basically the basic outline for the classes and their member attibutes, constructors, and methods.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -40,16 +40,29 @@ public class Person
     }
 }
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        DateTime theCurrentTime = DateTime.Now;
-        string date = theCurrentTime.ToShortDateString();
-        string time = theCurrentTime.ToShortTimeString();
-        Console.WriteLine($"The current date is {date}, and the hour is {time}");
+class Program {
+    static void Main(string[] args) {
+        string x = "PRINT THIS";
+        Console.WriteLine(x.ToLower());
+        // string LowerCaseX = x.ToLower();
+        Console.ReadLine();
+        Console.Clear();
+        Console.Beep();   //I found this through my own experimentation and will see how I can use it
+        // Console.SetWindowSize(50,50);  // I don't know what this does, but I plan to find out.
     }
 }
+
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         DateTime theCurrentTime = DateTime.Now;
+//         string date = theCurrentTime.ToShortDateString();
+//         string time = theCurrentTime.ToShortTimeString();
+//         Console.WriteLine($"The current date is {date}, and the hour is {time}");
+//     }
+// }
 
 // So, as it turns out, you need to define custom classes outside of the thing designated "class program".
 // Aside from that, I do believe that I figured it out.
